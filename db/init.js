c307d3dc5310da1b65407c585c9698ba33961c1a
Carro <robertmyers@hey.com>
2022-06-13 12:48:36 -0500

    added faultdb

--- db/init.js
+++ db/init.js
@@ -12,7 +12,7 @@ const ormconfig = {
   logging: ["query", "error", "schema"],
   host: process.env.DB_HOST || 'localhost',
   port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 5432,
-  database: process.env.DB_NAME || 'postgres',
+  database: process.env.DB_NAME || 'defaultdb',
   username: process.env.DB_USER || 'postgres',
   password: process.env.DB_PASS || 'postgres'
 }
