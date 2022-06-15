48bf76c1157ab859004459f3ddf86cccb447ba1c
Carro <robertmyers@hey.com>
2022-06-15 15:39:56 -0500

    you gotta build it first rofl

--- src/processor.ts
+++ src/processor.ts
@@ -17,109 +17,84 @@ processor.setDataSource({
   
 });
 
-processor.setTypesBundle({
-  "types": {
-      "Balance": "u64",
-      "NeuronMetadataOf": {
-          "version": "u32",
-          "ip": "u128", 
-          "port": "u16", 
-          "ip_type": "u8", 
-          "uid": "u32", 
-          "modality": "u8", 
-          "hotkey": "AccountId", 
-          "coldkey": "AccountId", 
-          "active": "u32",
-          "last_update": "u64",
-          "priority": "u64",
-          "stake": "u64",
-          "rank": "u64",
-          "trust": "u64",
-          "consensus": "u64",
-          "incentive": "u64",
-          "dividends": "u64",
-          "emission": "u64",
-          "bonds": "Vec<(u32, u64)>",
-          "weights": "Vec<(u32, u32)>"
-      }
-  }
-});
-
-processor.addEventHandler("balances.Transfer", async (ctx) => {
-  const transfer = getTransferEvent(ctx);
-  const tip = ctx.extrinsic?.tip || 0n;
-  const from = ss58.codec("kusama").encode(transfer.from);
-  const to = ss58.codec("kusama").encode(transfer.to);
-
-  const fromAcc = await getOrCreate(ctx.store, Account, from);
-  fromAcc.balance = fromAcc.balance || 0n;
-  fromAcc.balance -= transfer.amount;
-  fromAcc.balance -= tip;
-  await ctx.store.save(fromAcc);
-
-  const toAcc = await getOrCreate(ctx.store, Account, to);
-  toAcc.balance = toAcc.balance || 0n;
-  toAcc.balance += transfer.amount;
-  await ctx.store.save(toAcc);
-
-  await ctx.store.save(
-    new HistoricalBalance({
-      id: `${ctx.event.id}-to`,
-      account: fromAcc,
-      balance: fromAcc.balance,
-      date: new Date(ctx.block.timestamp),
-    })
-  );
-
-  await ctx.store.save(
-    new HistoricalBalance({
-      id: `${ctx.event.id}-from`,
-      account: toAcc,
-      balance: toAcc.balance,
-      date: new Date(ctx.block.timestamp),
-    })
-  );
-});
+processor.setTypesBundle('types.json');
 
 processor.run();
 
-interface TransferEvent {
-  from: Uint8Array;
-  to: Uint8Array;
-  amount: bigint;
-}
 
-function getTransferEvent(ctx: EventHandlerContext): TransferEvent {
-  const event = new BalancesTransferEvent(ctx);
-  if (event.isV1020) {
-    const [from, to, amount] = event.asV1020;
-    return { from, to, amount };
-  } else if (event.isV1050) {
-    const [from, to, amount] = event.asV1050;
-    return { from, to, amount };
-  } else {
-    const { from, to, amount } = event.asV9130;
-    return { from, to, amount };
-  }
-}
+// processor.addEventHandler("balances.Transfer", async (ctx) => {
+//   const transfer = getTransferEvent(ctx);
+//   const tip = ctx.extrinsic?.tip || 0n;
+//   const from = ss58.codec("kusama").encode(transfer.from);
+//   const to = ss58.codec("kusama").encode(transfer.to);
 
-async function getOrCreate<T extends { id: string }>(
-  store: Store,
-  EntityConstructor: EntityConstructor<T>,
-  id: string
-): Promise<T> {
-  let entity = await store.get<T>(EntityConstructor, {
-    where: { id },
-  });
+//   const fromAcc = await getOrCreate(ctx.store, Account, from);
+//   fromAcc.balance = fromAcc.balance || 0n;
+//   fromAcc.balance -= transfer.amount;
+//   fromAcc.balance -= tip;
+//   await ctx.store.save(fromAcc);
 
-  if (entity == null) {
-    entity = new EntityConstructor();
-    entity.id = id;
-  }
+//   const toAcc = await getOrCreate(ctx.store, Account, to);
+//   toAcc.balance = toAcc.balance || 0n;
+//   toAcc.balance += transfer.amount;
+//   await ctx.store.save(toAcc);
 
-  return entity;
-}
+//   await ctx.store.save(
+//     new HistoricalBalance({
+//       id: `${ctx.event.id}-to`,
+//       account: fromAcc,
+//       balance: fromAcc.balance,
+//       date: new Date(ctx.block.timestamp),
+//     })
+//   );
 
-type EntityConstructor<T> = {
-  new (...args: any[]): T;
-};
+//   await ctx.store.save(
+//     new HistoricalBalance({
+//       id: `${ctx.event.id}-from`,
+//       account: toAcc,
+//       balance: toAcc.balance,
+//       date: new Date(ctx.block.timestamp),
+//     })
+//   );
+// });
+
+// interface TransferEvent {
+//   from: Uint8Array;
+//   to: Uint8Array;
+//   amount: bigint;
+// }
+
+// function getTransferEvent(ctx: EventHandlerContext): TransferEvent {
+//   const event = new BalancesTransferEvent(ctx);
+//   if (event.isV1020) {
+//     const [from, to, amount] = event.asV1020;
+//     return { from, to, amount };
+//   } else if (event.isV1050) {
+//     const [from, to, amount] = event.asV1050;
+//     return { from, to, amount };
+//   } else {
+//     const { from, to, amount } = event.asV9130;
+//     return { from, to, amount };
+//   }
+// }
+
+// async function getOrCreate<T extends { id: string }>(
+//   store: Store,
+//   EntityConstructor: EntityConstructor<T>,
+//   id: string
+// ): Promise<T> {
+//   let entity = await store.get<T>(EntityConstructor, {
+//     where: { id },
+//   });
+
+//   if (entity == null) {
+//     entity = new EntityConstructor();
+//     entity.id = id;
+//   }
+
+//   return entity;
+// }
+
+// type EntityConstructor<T> = {
+//   new (...args: any[]): T;
+// };
